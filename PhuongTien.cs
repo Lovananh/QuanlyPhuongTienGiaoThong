586133ddfa28bacdbd442c394a28404d83cb9a8f
Lò Văn Anh <anhlo2819@gmail.com>
2024-10-08 08:59:07 +0700

    Add project files.

---
+++ PhuongTien.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanlyPhuongTienGiaoThong
+{
+    public abstract class  PhuongTien
+    {
+        public string TenPhuongTien {  get; set; }
+        public string LoaiNhienLieu { get; set; }
+        public PhuongTien(string TenPhuongTien, string LoaiNhienLieu)
+        {
+            this.TenPhuongTien = TenPhuongTien;
+            this.LoaiNhienLieu = LoaiNhienLieu;
+        }
+        public abstract void DiChuyen();
+        
+    }
+    interface IThongTinThem
+    {
+        int TocDoToiDa();
+        double MucTieuThuNhienLieu();
+    }
+    class XeHoi : PhuongTien, IThongTinThem
+    {
+        public XeHoi(string TenPhuongTien, string LoaiNhienLieu): base(TenPhuongTien, LoaiNhienLieu)
+        {
+
+        }
+        public override void DiChuyen()
+        {
+            Console.WriteLine($"{TenPhuongTien} dang di chuyen bang nhien lieu {LoaiNhienLieu}");
+        }
+        public int TocDoToiDa()
+        {
+           
+            return 200;
+        }
+        public double MucTieuThuNhienLieu()
+        {
+            return 8;
+        }
+    }
+    class XeDap : PhuongTien , IThongTinThem
+    {
+        public XeDap(string TenPhuongTien) : base(TenPhuongTien,"khong su dung nang luong")
+        {
+
+        }
+        public override void DiChuyen()
+        {
+            Console.WriteLine($"{TenPhuongTien} dang di chuyen bang suc nguoi");
+        }
+        public int TocDoToiDa()
+        {
+            return 25;// toc do tai da  
+        }
+        public double MucTieuThuNhienLieu()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using QuanlyPhuongTienGiaoThong;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<PhuongTien> danhSachPhuongTien = new List<PhuongTien>();
+
+        Console.WriteLine("Nhap so luong xe hoi:");
+        int soLuongXeHoi = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < soLuongXeHoi; i++)
+        {
+            Console.WriteLine("Nhap ten xe hoi:");
+            string tenPhuongTien = Console.ReadLine();
+
+            Console.WriteLine("Nhap loai nhien lieu:");
+            string loaiNhienLieu = Console.ReadLine();
+
+            danhSachPhuongTien.Add(new XeHoi(tenPhuongTien, loaiNhienLieu));
+        }
+
+        Console.WriteLine("Nhap so luong xe dap:");
+        int soLuongXeDap = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < soLuongXeDap; i++)
+        {
+            Console.WriteLine("Nhap ten xe dap:");
+            string tenPhuongTien = Console.ReadLine();
+
+            danhSachPhuongTien.Add(new XeDap(tenPhuongTien));
+        }
+        foreach (var phuongTien in danhSachPhuongTien)
+        {
+            phuongTien.DiChuyen();
+            if (phuongTien is IThongTinThem thongTinThem)
+            {
+                Console.WriteLine($"Toc do toi da: {thongTinThem.TocDoToiDa()} km/h");
+                try
+                {
+                    Console.WriteLine($"Muc tieu thu nang luong: {thongTinThem.MucTieuThuNhienLieu()} lit/100km");
+                }
+                catch (NotImplementedException)
+                {
+                    Console.WriteLine("Xe dap khong su dung nang luong.");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
